e47611c25309b8bd06b2766f779ff09489fc61d0
Jan Michael <jmwanzer@gmail.com>
2020-04-07 19:58:28 +0800

    updated port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var ex = require("express");
 var ej = require("ejs");
 var app = ex();
-
+var port = process.env.PORT || 3000;
 app.use(ex.static("public"));
 app.get("/", function (req, res) {
   res.render("index.ejs");
 });
-app.listen("3000", function () {
+app.listen(port, function () {
   console.log("Server is running");
 });
